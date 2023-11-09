155270d8fbbf9cac9f797a9b0801b5a4d71c0501
kimsojeong <rtw0202@naver.com>
2023-11-09 13:50:48 +0900

    멜론 차트 따오는 크롤링 코드 확인

--- movie_crawl/movie_crawl/pipelines.py
+++ movie_crawl/movie_crawl/pipelines.py
@@ -8,6 +8,39 @@
 from itemadapter import ItemAdapter
 
 
-class MovieCrawlPipeline:
+import os
+import csv
+
+
+class CsvPipeline:
+    def __init__(self):
+        self.files = None
+
+    def open_spider(self, spider):
+        self.files = {}
+
+    def close_spider(self, spider):
+        for site_name, (file, writer) in self.files.items():
+            file.close()
+            current_path = f"{site_name}.csv"
+            new_path = os.path.join('output', f"{site_name}.csv")
+            os.rename(current_path, new_path)
+
     def process_item(self, item, spider):
+        site_name = item['site']
+
+        if site_name not in self.files:
+            self.create_csv_file(site_name)
+
+        file, writer = self.files[site_name]
+        writer.writerow([item['rank'], item['title'], item['artist']])
+
         return item
+
+    def create_csv_file(self, site_name):
+        filename = f"{site_name}.csv"
+        file = open(filename, 'w', newline='', encoding='utf-8')
+        writer = csv.writer(file)
+        writer.writerow(['Rank', 'Title', 'Artist'])  # Header
+
+        self.files[site_name] = (file, writer)
---
+++ movie_crawl/movie_crawl/spiders/movie_spider.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+import scrapy
+import time
+
+
+class MovieSpider(scrapy.Spider):
+    name = "movie"
+
+    def start_requests(self):
+        urls = [
+            "https://www.melon.com/chart/",
+            "https://www.melon.com/chart/hot100/",
+            "https://www.melon.com/chart/day/",
+            "https://www.melon.com/chart/week/",
+            "https://www.melon.com/chart/month/",
+        ]
+        num_documents = 0
+        start_time = time.time()
+        # 페이지 순회
+        for url in urls:
+            num_documents += 1
+            yield scrapy.Request(url=url, callback=self.parse, meta={'site': url})
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"수집된 문서 수 : {num_documents}")
+        print(f"수집하는 데 걸린 시간 : {elapsed_time} seconds")
+
+    def parse(self, response):
+        site_name = response.meta.get('site', '').split('/')[-2]
+
+        music_sels_50 = response.css('tbody > tr.lst50')
+        music_sels_100 = response.css('tbody > tr.lst100')
+
+        # 50위 곡 정보 저장
+        for music_sel in music_sels_50:
+            item = {
+                'site': site_name,
+                'rank': music_sel.css('span.rank::text').get(),
+                'title': music_sel.css('div.ellipsis.rank01 > span > a::text').get(),
+                'artist': music_sel.css('div.ellipsis.rank02 > span > a::text').get()
+            }
+            yield item
+
+        # 100위 곡 정보 저장
+        for music_sel in music_sels_100:
+            item = {
+                'site': site_name,
+                'rank': music_sel.css('span.rank::text').get(),
+                'title': music_sel.css('div.ellipsis.rank01 > span > a::text').get(),
+                'artist': music_sel.css('div.ellipsis.rank02 > span > a::text').get()
+            }
+            yield item
